f4efa1319ff599d968e08765fb603e3ca08ca67d
ahmadrabea <85999137+ahmadrabea@users.noreply.github.com>
2022-01-23 02:22:08 +0200

    Update main.py
    gtts not working

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 import playsound
 import os
 import random
-from gtts import gTTS
+from gtts import gTTS #not working on windows (no problem found on mac & linux)  you can use (pyTTsx) instead
 import webbrowser
 
 
@@ -35,35 +35,35 @@ def record_audio(ask=False):
 #     # tts.save(audio_file)
 #     playsound.playsound(audio_string)
 
-
+#replace lara_speak function with (print) because of some compatibility problems in Google text to speech library with windows commands (no problem found on mac & linux)
 def respond(voice_data):
     if "name" in voice_data:
-        print("my name is lara")
+        print("my name is lara") #replaced
 
     if "what time is it" in voice_data:
-        print(ctime())
+        print(ctime()) #replaced
     if "search" in voice_data:
 
         search = record_audio("what do you want to search for ?")
         url = "https://google.com/search?q=" + search
         webbrowser.get().open(url)
-        print("here is what i found for " + search)
+        print("here is what i found for " + search) #replaced
 
     if "find a location" in voice_data:
 
         location = record_audio("what is the location you are looking for")
         url = "https://google.nl/maps/place/" + location + "/&amp;"
         webbrowser.get().open(url)
-        print("here is the location of " + location)
+        print("here is the location of " + location) #replaced
 
     if "sleep" in voice_data:
-        print("good bye")
+        print("good bye") #replaced
 
         exit()
 
 
 time.sleep(1)
-print("how can I help you ?")
+print("how can I help you ?") #replaced
 while 1:
     voice_data = record_audio()
     respond(voice_data)
